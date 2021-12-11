1afd652d3130e6373d3e7e67f9b81fd72529f422
Michal Pol <dissected9@gmail.com>
2021-12-11 21:17:10 +0100

    Added heroku and trello API

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   const apiRoot = 'https://kodilla-tasks-majkopolo.herokuapp.com/v1/task/';
+   const apiRoot = 'https://kodilla-tasks-majkopolo.herokuapp.com/v1/';
    const trelloApiRoot = 'https://kodilla-tasks-majkopolo.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
